1ec609a4ddb4249cfc73db18830d9884943b75eb
MMSantiagoMM <santiagomosquera2@gmail.com>
2023-05-22 16:15:27 -0500

    switch

---
+++ switch.js
@@ -0,0 +1,14 @@
+var numero = 1;
+switch (numero) {
+    case 1:
+        console.log("soy un uno")
+        break;
+    case 2:
+        console.log("Soy un dos")
+        break;
+    case 3:
+        console.log("Soy un tres")
+    default:
+        console.log("No soy ninguna de las opciones")
+        break;
+}
