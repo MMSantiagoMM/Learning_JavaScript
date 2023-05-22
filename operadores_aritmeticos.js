b95709ae9d021fa71762fe0af877f1bb2cf16418
MMSantiagoMM <santiagomosquera2@gmail.com>
2023-05-22 15:44:34 -0500

    Operadores aritméticos, lógicos,etc

---
+++ operadores_aritmeticos.js
@@ -0,0 +1,28 @@
+
+//Suma
+3 + 2
+//Resta
+50 - 10
+//Multiplicación
+10 * 45
+// División
+20/3
+// "=" Asignación
+var a = 1;
+//Valor de comparación "==" es flexible y retorna true
+3 == "3";
+// Valor de comparación "===" es más rígido. Retorna false
+3 === "3";
+//Valor de comparación 
+5 < 4; //false
+55 > 87; //false
+100 >= 45; //true
+
+//Valores de comparación
+a && b; //AND
+a || b; //OR
+
+//Aumento
+var edad = 40;
+edad++;
+edad+=2;
