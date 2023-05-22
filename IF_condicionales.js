03d97b921842f5ae4101e447912280f61ef5d5fc
MMSantiagoMM <santiagomosquera2@gmail.com>
2023-05-22 16:07:28 -0500

    Condicionales

---
+++ IF_condicionales.js
@@ -0,0 +1,19 @@
+if (5 > 7) {
+    console.log("Verdadero");
+} else {
+    console.log("Falso");
+}
+
+var edad = 18;
+
+if (edad === 18) {
+    console.log("Ey, puedes votar. Será tu primera vez");
+} else if (edad > 18) {
+    console.log("Puedes votar de nuevo");
+} else {
+    console.log("Aún no puedes votar")
+}
+//Operador ternario
+var numero = 1;
+
+var resultado = numero === 1 ? "soy igual" : "No son igual";
