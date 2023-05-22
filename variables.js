d1f75f2481e16b98dfbb67be592b2fdcad0ae4e4
MMSantiagoMM <santiagomosquera2@gmail.com>
2023-05-21 19:06:24 -0500

    definición y tipos de variables

---
+++ variables.js
@@ -0,0 +1,17 @@
+//Variable: Representación de un lugar en memoria para guardar valores. Para este ejemplo la variable se llama "nombre".
+var nombre = "Santiago";
+
+//Para declarar la variable
+var edad;
+//Inicializar la variable
+edad = 27;
+
+//Variable de array
+var elementos = ["computador","celuar","nevera","tv"];
+
+//Varianle tipo objeto
+var persona = {
+    nombre: "Santiago",
+    edad: 30
+}
+
